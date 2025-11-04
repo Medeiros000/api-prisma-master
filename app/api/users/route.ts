84d4ad8963a4347c3f6738eedbf316027a81449f
Júnior Medeiros <medeiros-000@hotmail.com>
2025-11-04 19:27:08 -0300

    refactor: remove unused vercel.json configuration; add prisma scripts to package.json

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,6 @@
 import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
-// Manipulador para o método GET em /api/users
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany();
 
